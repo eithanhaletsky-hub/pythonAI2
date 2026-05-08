8047a904125bc82d70533e5620bff02031132de4
Eithan <eithan.haletsky@gmail.com>
2026-05-08 16:05:36 +0300

    hh

--- pages/student_helper.py
+++ pages/student_helper.py
@@ -7,6 +7,7 @@ import streamlit as st
 
 
 
+
 # --- טעינת משתני סביבה ---
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
--- pages/trip_planner.py
+++ pages/trip_planner.py
@@ -10,6 +10,7 @@ import streamlit as st
 
 
 
+
 # --- טעינת משתני סביבה ---
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
